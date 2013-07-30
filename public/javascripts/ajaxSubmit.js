cbb740b638e2e0dcf5e6391a859089d225575bc7
David Handel <handeld@gmail.com>
2013-07-29 21:05:22 -0400

    AJAX works for moving from q to q, including answers. Need to reimplement move to thanks page.

--- public/javascripts/ajaxSubmit.js
+++ public/javascripts/ajaxSubmit.js
@@ -8,17 +8,11 @@ $(function() {
             url: '/' + currentQ,
             data: {answer: selected},
             success: function (data) {
-                $('p').text(data.question.q);
+                $('p#question').text(data.question.q);
                 $('#a1').contents()[1].nodeValue = data.question.a1;
-                // Alternative, without the use of .contents()
-                // $('#a1')[0].childNodes[1].nodeValue = data.question.a1;
-
-                //$('input#1').text(data.question.a1);
-                //$('label#1').append(data.question.a1);
-                //$('label#1').append(data.question.a1);
-                //$('input#1').parent().clear();
-                //$('input#1').parent().text('').append(data.question.a1);
-                //$('input#1').parent().append(data.question.a1);
+                $('#a2').contents()[1].nodeValue = data.question.a2;
+                $('#a3').contents()[1].nodeValue = data.question.a3;
+                $('#a4').contents()[1].nodeValue = data.question.a4;
             },
             dataType: 'json'
         });
